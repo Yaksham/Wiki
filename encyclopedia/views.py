46315e7724de8a3a7f79634c4ad7e1ec42fde25a
Yaksham <82596143+Yaksham@users.noreply.github.com>
2022-10-09 22:48:23 +0530

    fixed entries and implemented search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+from django.shortcuts import redirect
 from . import util
 
 
@@ -9,11 +10,8 @@ def index(request):
     })
 
 def entry(request, name):
-    if request.method == 'POST':
-        page = util.get_entry(request.POST.get("q"))
-    else:
-        page = util.get_entry(name)
-    if page != None:
+    page = util.get_entry(name)
+    if page is not None:
         markdowner = Markdown()
         content = markdowner.convert(page)
     else:
@@ -23,3 +21,24 @@ def entry(request, name):
             "content": content,
             "name": name
         })
+
+def search(request):
+    query = request.GET.get("q")
+    page = util.get_entry(query)
+    if page is not None:
+        return redirect("entry", name=query)
+    else:
+        matches = []
+        entries = util.list_entries()
+        for entry in entries:
+            if query.lower() in entry.lower():
+                matches.append(entry)
+        if not matches:
+            heading = "<h2>No reuslts found.</h2>"
+        else:
+            heading = "<h2>Did you mean:</h2>"
+        return render(request, "encyclopedia/search.html", {
+            "list": matches,
+            "heading": heading
+        })
+
